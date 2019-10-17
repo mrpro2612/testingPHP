dbfad98e79bbb24e779366bfd47389d049e85ac4
mrpro2612 <54970867+mrpro2612@users.noreply.github.com>
2019-10-17 10:05:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<title>PHP Test</title>
 </head>
 <body>
-	<?php echo '<p>Hello World</p>'; ?> 
+	<?php echo '<p style="color:red;">Hello World</p>'; ?>
+	<img src="~/15965450_732579930230786_2086714662629346510_n.jpg" alt="">
 </body>
 </html>
