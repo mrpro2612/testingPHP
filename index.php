9cae0ac4063f71f5a77751dde6367bab4955a4a4
mrpro2612 <54970867+mrpro2612@users.noreply.github.com>
2019-10-17 10:07:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 </head>
 <body>
 	<?php echo '<p style="color:red;">Hello World</p>'; ?>
-	<img src="~/15965450_732579930230786_2086714662629346510_n.jpg" alt="">
+	<img src="15965450_732579930230786_2086714662629346510_n.jpg" alt="">
 </body>
 </html>
